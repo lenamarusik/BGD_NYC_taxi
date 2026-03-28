d2bf8bd0478e79b66408e0c0a253f4dc2ea6cc33
Lena Marusik <108836218+lenamarusik@users.noreply.github.com>
2026-03-28 23:49:06 +0100

    Added data type to silver_to_gold_daily_revenue.sql

--- sql/gold/silver_to_gold_daily_revenue.sql
+++ sql/gold/silver_to_gold_daily_revenue.sql
@@ -5,9 +5,9 @@ SELECT
     pickup_date,
     COUNT(*) AS total_trips,
     SUM(total_amount) AS total_revenue,
-    AVG(total_amount) AS avg_trip_revenue,
-    AVG(trip_distance) AS avg_trip_distance,
-    AVG(trip_duration_minutes) AS avg_trip_duration_minutes
+    AVG(total_amount)::FLOAT8 AS avg_trip_revenue,
+    AVG(trip_distance)::FLOAT8 AS avg_trip_distance,
+    AVG(trip_duration_minutes)::FLOAT8 AS avg_trip_duration_minutes
 FROM silver.yellow_taxi_trips_2023_cleaned
 GROUP BY pickup_date
 ORDER BY pickup_date;
