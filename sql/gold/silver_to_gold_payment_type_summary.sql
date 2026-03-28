54f7a7466ea45fdd4fe370fe0b9fec1d61393167
Lena Marusik <108836218+lenamarusik@users.noreply.github.com>
2026-03-28 23:54:59 +0100

    Added data type to silver_to_gold_payment_type_summary.sql

--- sql/gold/silver_to_gold_payment_type_summary.sql
+++ sql/gold/silver_to_gold_payment_type_summary.sql
@@ -4,7 +4,7 @@ CREATE TABLE gold.payment_type_summary_2023 AS
 SELECT
     payment_type,
     COUNT(*) AS total_trips,
-    SUM(total_amount) AS total_revenue,
+    SUM(total_amount)::FLOAT8 AS total_revenue,
     AVG(total_amount)::FLOAT8 AS avg_total_amount,
     AVG(tip_amount)::FLOAT8 AS avg_tip_amount
 FROM silver.yellow_taxi_trips_2023_cleaned
