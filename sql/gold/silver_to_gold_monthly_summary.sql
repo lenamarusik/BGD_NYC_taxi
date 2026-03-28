d119248ef20f889359413807fa7709fd62ab8031
Lena Marusik <108836218+lenamarusik@users.noreply.github.com>
2026-03-28 23:53:08 +0100

    Added data type to silver_to_gold_monthly_summary.sql

--- sql/gold/silver_to_gold_monthly_summary.sql
+++ sql/gold/silver_to_gold_monthly_summary.sql
@@ -5,12 +5,12 @@ SELECT
     pickup_year,
     pickup_month,
     COUNT(*) AS total_trips,
-    SUM(total_amount) AS total_revenue,
-    AVG(total_amount) AS avg_total_amount,
-    AVG(fare_amount) AS avg_fare_amount,
-    AVG(tip_amount) AS avg_tip_amount,
-    AVG(trip_distance) AS avg_trip_distance,
-    AVG(trip_duration_minutes) AS avg_trip_duration_minutes
+    SUM(total_amount)::FLOAT8 AS total_revenue,
+    AVG(total_amount)::FLOAT8 AS avg_total_amount,
+    AVG(fare_amount)::FLOAT8 AS avg_fare_amount,
+    AVG(tip_amount)::FLOAT8 AS avg_tip_amount,
+    AVG(trip_distance)::FLOAT8 AS avg_trip_distance,
+    AVG(trip_duration_minutes)::FLOAT8 AS avg_trip_duration_minutes
 FROM silver.yellow_taxi_trips_2023_cleaned
 GROUP BY pickup_year, pickup_month
 ORDER BY pickup_year, pickup_month;
