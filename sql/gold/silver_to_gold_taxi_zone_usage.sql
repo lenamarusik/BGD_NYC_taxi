7334f0e890f0255bec428902c54c08fbd27ae22b
Lena Marusik <108836218+lenamarusik@users.noreply.github.com>
2026-03-29 10:10:35 +0200

    Added data types to silver_to_gold_taxi_zone_usage.sql

--- sql/gold/silver_to_gold_taxi_zone_usage.sql
+++ sql/gold/silver_to_gold_taxi_zone_usage.sql
@@ -4,16 +4,16 @@ CREATE TABLE gold.taxi_zone_usage AS
 WITH pickup_stats AS (
     SELECT
         pulocationid AS locationid,
-        COUNT(*) AS total_pickups,
-        SUM(total_amount) AS pickup_revenue
+        COUNT(*)::INT8 AS total_pickups,
+        SUM(total_amount)::FLOAT8 AS pickup_revenue
     FROM silver.yellow_taxi_trips_2023_cleaned
     GROUP BY pulocationid
 ),
 dropoff_stats AS (
     SELECT
         dolocationid AS locationid,
-        COUNT(*) AS total_dropoffs,
-        SUM(total_amount) AS dropoff_revenue
+        COUNT(*)::INT8 AS total_dropoffs,
+        SUM(total_amount)::FLOAT8 AS dropoff_revenue
     FROM silver.yellow_taxi_trips_2023_cleaned
     GROUP BY dolocationid
 )
@@ -22,10 +22,10 @@ SELECT
     z.borough,
     z.zone,
     z.service_zone,
-    COALESCE(p.total_pickups, 0) AS total_pickups,
-    COALESCE(d.total_dropoffs, 0) AS total_dropoffs,
-    COALESCE(p.pickup_revenue, 0) AS pickup_revenue,
-    COALESCE(d.dropoff_revenue, 0) AS dropoff_revenue
+    COALESCE(p.total_pickups, 0)::INT8 AS total_pickups,
+    COALESCE(d.total_dropoffs, 0)::INT8 AS total_dropoffs,
+    COALESCE(p.pickup_revenue, 0)::FLOAT8 AS pickup_revenue,
+    COALESCE(d.dropoff_revenue, 0)::FLOAT8 AS dropoff_revenue
 FROM silver.taxi_zone_lookup_cleaned z
 LEFT JOIN pickup_stats p
     ON z.locationid = p.locationid
