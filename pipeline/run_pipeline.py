8929e26746f3069732e1c11a4b8647857d2b96e8
Lena Marusik <108836218+lenamarusik@users.noreply.github.com>
2026-04-12 11:38:00 +0200

    Added running spark process

--- pipeline/run_pipeline.py
+++ pipeline/run_pipeline.py
@@ -53,6 +53,19 @@ def run_python_file(py_file: Path) -> None:
     )
 
 
+def run_spark_file(py_file: Path) -> None:
+    print(f"\nRunning Spark file: {py_file}")
+
+    subprocess.run(
+        [
+            "spark-submit",
+            str(py_file),
+        ],
+        check=True,
+        env=env,
+    )
+
+
 def main() -> None:
     # 1. Create schemas and raw tables
     run_sql_file(BASE_DIR / "sql" / "raw" / "create_schemas_and_raw_tables.sql")
@@ -61,17 +74,20 @@ def main() -> None:
     run_python_file(BASE_DIR / "scripts" / "load_to_raw_taxi_zone_lookup.py")
     run_python_file(BASE_DIR / "scripts" / "load_to_raw_yellow_taxi_trips_2023.py")
 
-    # 3. Transform RAW -> SILVER
+    # 3. Run scalable Spark processing
+    run_spark_file(BASE_DIR / "spark_jobs" / "yellow_taxi_spark_job.py")
+
+    # 4. Transform RAW -> SILVER
     run_sql_file(BASE_DIR / "sql" / "silver" / "raw_to_silver_taxi_zone_lookup.sql")
     run_sql_file(BASE_DIR / "sql" / "silver" / "raw_to_silver_yellow_taxi_trips_2023.sql")
 
-    # 4. Transform SILVER -> GOLD
+    # 5. Transform SILVER -> GOLD
     run_sql_file(BASE_DIR / "sql" / "gold" / "silver_to_gold_daily_revenue.sql")
     run_sql_file(BASE_DIR / "sql" / "gold" / "silver_to_gold_monthly_summary.sql")
     run_sql_file(BASE_DIR / "sql" / "gold" / "silver_to_gold_payment_type_summary.sql")
     run_sql_file(BASE_DIR / "sql" / "gold" / "silver_to_gold_taxi_zone_usage.sql")
 
-    # 5. Add constraints
+    # 6. Add constraints
     run_sql_file(BASE_DIR / "sql" / "gold" / "adding_pk_setting_nn.sql")
 
     print("\nPipeline finished successfully.")
